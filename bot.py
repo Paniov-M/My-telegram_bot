d34025f57efac92e25fad1f58a094d75edcdc42d
Paniov Maksym <panov_ak19@nuwm.edu.ua>
2023-11-14 20:18:28 +0200

    first_commit

--- bot.py
+++ bot.py
@@ -1,9 +1,62 @@
-from aiogram import executor
-from dispatcher import dp
-import handlers
+import telebot
+import sqlite3
 
-from db import BotDB
-BotDB = BotDB('accountant.db')
+bot = telebot.TeleBot("6033958862:AAGcyNNmgMj30czc5rQENIijg-dIsMAIpIw")
+name = None
 
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    conn=sqlite3.connect('itproger.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, name varchar(50),pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id,'Привет,як тебе звати?')
+    bot.register_next_step_handler(message,user_name)
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введіть пароль')
+    bot.register_next_step_handler(message, user_pass)
+
+def user_pass(message):
+
+    password = message.text.strip()
+    conn=sqlite3.connect('itproger.sql')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users (name,pass) VALUES('%s','%s')"%(name,password))
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton('Список користувачів',callback_data='users'))
+    bot.send_message(message.chat.id, 'Користувач зареєстрований',reply_markup=markup)
+    #bot.register_next_step_handler(message, user_pass)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback(call):
+
+    conn=sqlite3.connect('itproger.sql')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+
+    info = ''
+    for el in users:
+        info += f'Name:{el[1]}, Password: {el[2]}\n'
+
+    bot.send_message(call.message.chat.id, info)
+
+    cur.close()
+    conn.close()
+
+bot.polling(none_stop=True)
